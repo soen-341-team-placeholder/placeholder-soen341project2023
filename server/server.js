7153f04b555717a995b6c9b565396ad7b1e382b8
walidoow <walidoo_2002@hotmail.com>
2023-02-27 01:59:21 -0500

    Added POST method to /users to create new users

---
+++ server/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const mongoDB = 'mongodb+srv://' + process.env.MONGO_USERNAME + ':' + process.env.MONGO_PASSWORD + '@' + process.env.MONGO_HOST + '/' + process.env.MONGO_DB
+  + '?retryWrites=true&w=majority';
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Routers
+const userRouter = require('./routes/user');
+
+app.use(express.json());
+app.use('/users', userRouter);
+
+
+// Define app port
+app.listen(process.env.PORT, () => console.log('listening on port ' + process.env.PORT));
