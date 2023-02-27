a0964518a6c1f7077931b279ccd78bbe08280a89
Walid <walidoo_2002@hotmail.com>
2023-02-27 16:47:46 -0500

    Added /login path to the API (#47)

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const router = express.Router();
 
@@ -12,6 +13,7 @@ router.get('/', (req, res, next) => {
                 res.status(400).send('message:' + err);
                 console.log(err);
             } else if (user) {
+                user.password = undefined;
                 res.status(200).json(user);
             } else {
                 res.status(404).send('message: User not found');
@@ -21,8 +23,13 @@ router.get('/', (req, res, next) => {
     }
 
     // User has not provided an email, thus return all users
-    // Find all users
-    User.find({}).then((users) => {
+    User.find({}).then((users) => { // Find all users
+        
+        // Remove password from each user
+        users.forEach((user) => {
+            user.password = undefined;
+        });
+        
         res.status(200).json(users);
     }).catch((err) => {
         res.status(400).send('message:' + err);
@@ -30,14 +37,17 @@ router.get('/', (req, res, next) => {
     });
 });
 
-router.post('/', (req, res) => {
+// Create a new user
+router.post('/', async (req, res) => {
     const newUser = new User(req.body);
+    newUser.password = await bcrypt.hash(newUser.password, 10);
     newUser.save((err, user) => {
         if (err) {
             res.status(400).send('message:' + err);
             console.log(err);
         } else {
-            res.status(200).json(user);
+            user.password = undefined;
+            res.status(201).json(user);
         }
     });
 });
