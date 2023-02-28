56c126a0ff03c4b8c6e53cf5272452ddafc0ff47
vibqetowi <abutcher.usa@protonmail.com>
2023-02-28 15:13:24 -0500

    u

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,40 +1,40 @@
-import { useRef } from 'react';
-import { FaBars, FaDownload, FaTimes } from 'react-icons/fa'; // import font awesome icons
-import '../styles/Navbar.css';
-import { BrowserRouter as Router, 
-  Route, Link ,Routes} from 'react-router-dom';
+import { useRef } from "react";
+import { FaBars, FaDownload, FaTimes } from "react-icons/fa"; // import font awesome icons
+import "../styles/Navbar.css";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
- import SearchBar from './SearchBar';
+import SearchBar from "./SearchBar";
 
 function Navbar() {
-    const navRef = useRef(); // this variable will refer to the nav tag
+  const navRef = useRef(); // this variable will refer to the nav tag
 
-    /* each time this function is called, we will add/remove
+  /* each time this function is called, we will add/remove
     the class name from the nav tag */
-    const showNavbar = () => {
-        navRef.current.classList.toggle("responsive_nav");
-    };
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
-    return (
-        <header>
-            <h3>LOGO</h3>
-            <SearchBar />
-            <nav ref={navRef}>
-                <Link to ="/">Home</Link>
-                <Link to ="/">Jobs</Link>
-                <Link to = "/">Inbox</Link>
-                <Link to ="/profile/:userID">Profile</Link>
-                {/* button for full screen */}
-                <button className='nav-btn nav-close-btn' onClick={showNavbar}>
-                    <FaTimes />
-                </button>
-            </nav>
-            {/* button for smaller screen */}
-            <button className='nav-btn' onClick={showNavbar}>
-                <FaBars />
-            </button>
-        </header>
-    );
+  return (
+    <header>
+      <h3>LOGO</h3>
+      <SearchBar />
+      <nav ref={navRef}>
+        <Link to="/">Home</Link>
+        <Link to="/">Jobs</Link>
+        <Link to="/">Inbox</Link>
+        <Link to="/profile/:userID">Profile</Link>
+        <Link to="/about">About</Link>
+        {/* button for full screen */}
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      {/* button for smaller screen */}
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
 }
 
 export default Navbar;
