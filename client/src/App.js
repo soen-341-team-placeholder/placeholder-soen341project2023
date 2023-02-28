8f5e4d7538ac17ca5a6b8339f63b07deb6ab3f2f
vibqetowi <abutcher.usa@protonmail.com>
2023-02-28 14:27:13 -0500

    u

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,44 @@
 import React from 'react';
-import { BrowserRouter as Router, 
-  Route, Link ,Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Navbar from './components/Navbar';
+
 import ApplicantProfilePage from './pages/ApplicantProfilePage';
+import HomePage from "./pages/HomePage"
 
-
-export default function App()  {
-
+export default function App() {
   return (
     <>
       <Router>
-          <header>
-        <Navbar />
-          </header>
-          <main>
-            <div className="center-contents">    
-                  <span>
-                    <Link to="/about" className="not-real-site-txt">
-                      This is a span
-                    </Link>{" "}
-                  </span>
-            </div>
+        <header>
+          <Navbar />
+        </header>
 
-                <Routes>
-          <Route path="/" render={() => (
-            <div className="max-w-4xl mx-auto px-4 py-8">
-              <h1 className="text-4xl font-bold mb-4">Welcome to my React app</h1>
-              <p className="mb-4">This is the homepage.</p>
-              <Link to="/123456" className="text-blue-500 hover:text-blue-600">View my online CV</Link>
+        <main>
+          <div className="center-contents">
+            <div>
+              <Link to="/about" className="not-real-site-txt">
+                This is a link
+              </Link>{" "}
             </div>
-          )} />
+          </div>
+
+          <Route exact path="/" component ={HomePage}/>
           <Route path="/profile/:userID" component={ApplicantProfilePage} />
-              </Routes>
-          </main>
+          
+        </main>
 
-          <footer>
-            <br></br>
-            {/* <h1 className="square-logo">DR</h1> */}
+        <footer>
+          <br></br>
+          {/* <h1 className="square-logo">DR</h1> */}
 
-            <div className="text-center">
-              <p className="copyright-txt">
-                {" "}
-                &#169; Copyright text here
-              </p>
-            </div>
-          </footer>
+          <div className="text-center">
+            <p className="copyright-txt">
+              {" "}
+              &#169; Copyright text here
+            </p>
+          </div>
+        </footer>
       </Router>
     </>
   );
 }
-
