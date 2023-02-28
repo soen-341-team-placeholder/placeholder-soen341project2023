caceb0eca27d8a1b15a76dc1f21f2d6d9f01b877
vibqetowi <abutcher.usa@protonmail.com>
2023-02-28 11:20:03 -0500

    debug

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
-import ApplicantProfilePage from "./pages/ApplicantProfilePage"
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Navbar from './components/Navbar';
+import ApplicantProfilePage from "./pages/ApplicantProfilePage";
 
 function App() {
   return (
@@ -14,11 +19,16 @@ function App() {
         </React.Fragment>
         <p>hello world</p>
          <div className="appRoutes">
-      <Router>
+       <Router>
         <Switch>
-          <Routes>
-          <Route path="/applicant/profile/:userID" component={ApplicantProfilePage} />
-        </Routes>
+          <Route exact path="/" render={() => (
+            <div className="max-w-4xl mx-auto px-4 py-8">
+              <h1 className="text-4xl font-bold mb-4">Welcome to my React app</h1>
+              <p className="mb-4">This is the homepage.</p>
+              <Link to="/123456" className="text-blue-500 hover:text-blue-600">View my online CV</Link>
+            </div>
+          )} />
+          <Route path="profile/:userID" component={ApplicantProfilePage} />
         </Switch>
       </Router>
     </div>
