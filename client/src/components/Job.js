41064088449d82340006599798d403a7005fbccf
walidoow <walidoo_2002@hotmail.com>
2023-03-01 00:33:34 -0500

    Added render of postings from API

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -2,27 +2,29 @@ import React, { useEffect, useState } from "react";
 import { FaPlay } from "react-icons/fa";
 import '../styles/Job.css';
 
-export default function Job(){
+export default function Job({ title, location, salary, description }) {
 
-    return(
+
+    console.log(title, location, salary, description);
+    return (
         <div className="post-container">
             <div className="logo">
-                <FaPlay style={{fontSize:"3em", color:"white"}}/>
+                <FaPlay style={{ fontSize: "3em", color: "white" }} />
                 {/* <img src="/images/postIcon.jpg" alt="img"></img> */}
             </div>
             <div className="part1">
                 <div className="company">
-                <span className="cname">"Company name"</span> 
+                    <span className="cname">"Company name"</span>
                 </div>
-                <div className="title-position">"Title/Position"</div>
+                <div className="title-position">{title}</div>
                 <div className="details">
-                    <span>"Salary"</span>
+                    <span>${salary}/h</span>
                     <span>&nbsp;•&nbsp;</span>
-                    <span>"location"</span>
+                    <span>Location: {location}</span>
                 </div>
             </div>
             <div className="part2">
-                <span className="description">Lorem ipsum dolor sit amet, in pro justo aliquid epicurei. Percipit signiferumque ne mei, volumus facilisi deseruisse eu qui. Esse singulis disputando ne mea, ne odio convenire eos. Duo mucius omittam offendit no.</span>
+                <span className="description">{description}</span>
             </div>
         </div>
     );
