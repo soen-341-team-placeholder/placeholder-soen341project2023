3ae19813295a36148fbdcdcf879611fcfa79a829
walidoow <walidoo_2002@hotmail.com>
2023-03-22 14:07:34 -0400

    Retrieve user from the API on EditStudentPage.js

--- client/src/pages/EditStudentPage.js
+++ client/src/pages/EditStudentPage.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+import Cookies from "universal-cookie";
 import Student from '../components/student/Student';
-import { useParams } from 'react-router-dom';
+import {useParams} from 'react-router-dom';
+
+const cookies = new Cookies();
 
 function EditStudentPage() {
 
-  const userId = useParams().userId;
-  const user = {
-    firstName: "Youssef",
-    lastName: "Alsheghri",
-    email: "youssef.alsheghri@gmail.com",
-    biography: "Software Engineering Major at Concordia University",
-    location: "Montreal, Canada"
-  }
+    const userId = useParams().userId;
 
-  return (
-    <div>
-      <React.Fragment>
-        <Student user={user} />
-      </React.Fragment>
-    </div>
-  );
+    const [user, setUser] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:4000/users/' + userId, {
+            headers: {
+                authorization: `Bearer ${cookies.get('accessToken')}`
+            }
+        }).then((res) => {
+            setUser(res.data);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }, []);
+
+    return (<div>
+        <React.Fragment>
+            <Student user={user}/>
+        </React.Fragment>
+    </div>);
 }
 
 export default EditStudentPage;
