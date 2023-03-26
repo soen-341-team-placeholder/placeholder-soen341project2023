41370448321778997ca9b97d72571707824b42cc
wade3hamati <wade3_hamati@outlook.com>
2023-03-25 20:47:15 -0400

    made the subscribe endpoint so users can receive emails when their favorite employer posts a new job

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,67 +5,60 @@ const authenticateToken = require('../auth/token_validator')
 const router = express.Router()
 
 router.get('/', authenticateToken, async (req, res) => {
-    const email = req.query.email;
+    const email = req.query.email
 
-    // Get requester user type and email
-    const userEmail = req.requester.email;
-    const userType = req.requester.userType;
-    
     // User has provided an email, thus search for a specific user with that email
     if (email != null) {
-
-        if (userType === 'admin' || userEmail === email) {
-            User.findOne({ email: email }, (err, user) => {
-                if (err) {
-                    res.status(400).send('message:' + err);
-                    console.log(err);
-                } else if (user) {
-                    user.password = undefined;
-                    res.status(200).json(user);
-                } else {
-                    res.status(404).send('message: User not found');
-                }
-            });
-        }
-        else {
-            res.status(403).send('message: Forbidden');
-        }
-        return;
+        User.findOne({ email: email }, (err, user) => {
+            if (err) {
+                res.status(400).send('message:' + err)
+                console.log(err)
+            } else if (user) {
+                user.password = undefined
+                res.status(200).json(user)
+            } else {
+                res.status(404).send('message: User not found')
+            }
+        })
+        return
     }
 
     // User has not provided an email, thus return all users
+
+    // Get requester user type
+    const userType = (await User.findOne({ email: req.email })).userType
     if (userType != 'admin') {
-        res.status(403).send('message: Forbidden');
-        return;
+        res.status(403).send('message: Forbidden')
+        return
     }
 
     User.find({}).then((users) => { // Find all users
         // Remove password from each user
         users.forEach((user) => {
-            user.password = undefined;
-        });
-        res.status(200).json(users);
+            user.password = undefined
+        })
+        res.status(200).json(users)
 
     }).catch((err) => {
-        res.status(400).send('message:' + err);
-        console.log(err);
-    });
-});
+        res.status(400).send('message:' + err)
+        console.log(err)
+    })
+})
 
 // Create a new user
 router.post('/', async (req, res) => {
-    const newUser = new User(req.body);
-    newUser.password = await bcrypt.hash(newUser.password, 10);
+    const newUser = new User(req.body)
+    newUser.password = await bcrypt.hash(newUser.password, 10)
     newUser.save((err, user) => {
         if (err) {
-            res.status(400).send('message:' + err);
-            console.log(err);
+            res.status(400).send('message:' + err)
+            console.log(err)
         } else {
-            user.password = undefined;
-            res.status(201).json(user);
+            user.password = undefined
+            res.status(201).json(user)
         }
-    });
-});
+    })
+})
 
 // get user by id
 router.get('/:id', getUser, authenticateToken, async (req, res) => {
@@ -82,43 +75,71 @@ router.patch('/', async (req, res) => {
 })
 
 //update user by id
-router.patch('/:id', authenticateToken, getUser, async (req, res) => {
-    const id = req.params.id;
-    const { firstName, lastName, age, userType } = req.body;
-
+router.patch('/:id', authenticateToken, async (req, res) => {
+    const newEmployer = req.body.subscribedTo;
+    const newSavedPosting = req.body.savedPostings;
+    const userId = req.params.id;
     try{
-    const id = req.params.id;
-    const { firstName, lastName, age, userType } = req.body;
-  
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: id },
-      { firstName, lastName,age, userType },
-      { new: true } // return the updated document
-    )
-  
-    if (!updatedUser) {
-      return res.status(404).send(`User with ID ${id} not found`);
-    }
-  
-    res.send(`User with ID ${id} has been updated`);
+        const user = await User.findById(userId);
+        const { firstName, lastName, age, userType } = req.body;
+        const updatedUser = await User.findOneAndUpdate(
+            { _id: userId },
+            { firstName, lastName,age, userType },
+            { new: true } // return the updated document
+        )
+        if (!updatedUser) {
+            return res.status(404).send(`User with ID ${userId} not found`);
+        }
+
+        const existingEmployers = user.subscribedTo;
+        const uniqueEmployers = [];
+        const existingSavedPostings = user.savedPostings;
+        const uniqueSavedPostings = [];
+
+        newEmployer.forEach(name => {
+            if (!existingEmployers.includes(name)) {
+                uniqueEmployers.push(name);
+            }
+          });
+
+        if (uniqueEmployers.length === 0) {
+            res.status(400).send('All names already exist');
+            return;
+        }
+
+        newEmployer.forEach(name => {
+            if (!existingSavedPostings.includes(name)) {
+                uniqueSavedPostings.push(name);
+            }
+          });
+
+        if (uniqueSavedPostings.length === 0) {
+            res.status(400).send('All names already exist');
+            return;
+        }
+        
+        user.subscribedTo = [...existingEmployers, ...uniqueEmployers];
+        user.savedPostings = [...user.existingSavedPostings, ...uniqueSavedPostings];
+        await user.save();
+        res.send(`User with ID ${userId} has been updated`);
+
     } catch (error) {
-    console.error(error);
-    res.status(500).send('Server error')
+        console.error(error);
+        res.status(500).send('Server error')
     }
 })
 
-router.delete('/:id', authenticateToken, getUser, async (req, res) => {
-
-    const id = req.params.id;
+router.delete('/:id', authenticateToken, async (req, res) => {
+    const userId = req.params.id;
     try {
         // Find and delete the user with the specified ID from the database
-        const deletedUser = await User.findOneAndDelete({ _id: id });
+        const deletedUser = await User.findOneAndDelete({ _id: userId });
 
         if (!deletedUser) {
-            return res.status(404).send(`User with ID ${id} not found`);
+            return res.status(404).send(`User with ID ${userId} not found`);
         }
 
-        res.send(`User with ID ${id} has been deleted`);
+        res.send(`User with ID ${userId} has been deleted`);
     } catch (error) {
         console.error(error);
         res.status(500).send('Server error');
@@ -128,7 +149,6 @@ router.delete('/:id', authenticateToken, getUser, async (req, res) => {
 
 //find user by id
 async function getUser(req, res, next) {
-    console.log("get user")
     let user
     try {
         user = await User.findById(req.params.id)
