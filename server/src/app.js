99999f0f4faa3217690f3657d20841e15738c34c
walidoow <31325808+walidoow@users.noreply.github.com>
2023-04-10 17:36:38 -0400

    Updated backend for unit tests

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,8 @@
 require('dotenv').config();
 
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
-const mongoDB = 'mongodb+srv://' + process.env.MONGO_USERNAME + ':' + process.env.MONGO_PASSWORD + '@' + process.env.MONGO_HOST + '/' + process.env.MONGO_DB
-    + '?retryWrites=true&w=majority';
 const cors = require("cors");
-
-mongoose.set('strictQuery', true);
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Routers
 const usersRouter = require('./routes/users');
