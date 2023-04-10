2c0c9ab6e040dbc9bb8cd14537090a061f9d073e
walidoow <31325808+walidoow@users.noreply.github.com>
2023-04-10 17:36:38 -0400

    Updated backend for unit tests

--- client/src/components/popups/PostingPopup.js
+++ client/src/components/popups/PostingPopup.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import "../styles/PostingPopup.css";
+import "../../styles/PostingPopup.css;
 import PostingPopupInput from "./PostingPopupInput";
 import {FaRegTimesCircle} from "react-icons/fa";
 import axios from "axios";
