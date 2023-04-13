979547aac4496160fd1f387b0fe74b13d092c30c
vibqetowi i <abutcher.usa@protonmail.com>
2023-04-13 21:59:47 +0000

    failed test, needs owner probably

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './public/index.html',
+  ],
   theme: {
     extend: {},
   },
--- deploy.js
+++ deploy.js
@@ -1,13 +1,21 @@
 const ghpages = require('gh-pages');
 const path = require('path');
 
+const repo = 'https://github.com/walidoow/placeholder-soen341project2023.git';
+const branch = 'gh-pages';
+
 ghpages.publish(
-  path.join(__dirname, 'build'),
+  path.join(__dirname, 'client', 'build'),
   {
-    branch: 'gh-pages',
-    repo: 'https://github.com/walidoow/placeholder-soen341project2023.git',
+    branch: branch,
+    repo: repo,
   },
-  () => {
-    console.log('Deploy Complete!')
+  (err) => {
+    if (err) {
+      console.error('Deploy Failed!', err);
+    } else {
+      const link = `https://${repo.split('/')[3]}.github.io/${repo.split('/')[4].split('.')[0]}/`;
+      console.log(`Deploy Complete! View your app at: ${link}`);
+    }
   }
 );
