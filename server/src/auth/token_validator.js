554bcb35526ca9b530b478535f261a06d1b1af8a
vibqetowi <abutcher.usa@protonmail.com>
2023-04-14 11:25:55 -0400

    test introduce security issue

--- server/src/auth/token_validator.js
+++ server/src/auth/token_validator.js
@@ -1,18 +1,41 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+// const jwt = require('jsonwebtoken');
+// const User = require('../models/User');
+
+// // Middleware to authenticate token
+// function authenticateToken(req, res, next) {
+//     const authHeader = req.headers['authorization'];
+//     const token = authHeader && authHeader.split(' ')[1];
+//     if (token == null) return res.sendStatus(401);
+
+//     jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET, async (err) => {
+//         if (err) return res.status(403).send('message: Forbidden (Invalid token)');
+//         req.email = (await jwt.decode(token)).email.toLowerCase();
+//         req.requester = await User.findOne({ email: req.email });
+//         next();
+//     });
+// }
+
+// module.exports = authenticateToken;
+
+
+// ____________________________________________________________
+// test insecure
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 // Middleware to authenticate token
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (token == null) return res.sendStatus(401);
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
 
-    jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET, async (err) => {
-        if (err) return res.status(403).send('message: Forbidden (Invalid token)');
-        req.email = (await jwt.decode(token)).email.toLowerCase();
-        req.requester = await User.findOne({ email: req.email });
-        next();
-    });
+  jwt.verify(token, "insecure_secret_key", async (err) => {
+    // using an insecure secret key
+    if (err) return res.status(403).send("message: Forbidden (Invalid token)");
+    req.email = (await jwt.decode(token)).email.toLowerCase();
+    req.requester = await User.findOne({ email: req.email });
+    next();
+  });
 }
 
 module.exports = authenticateToken;
